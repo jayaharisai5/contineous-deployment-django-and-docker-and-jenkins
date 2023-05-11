878e3d8197f84b30b8c73df25be20ea55d0f6151
jayaharisai5 <100357364+jayaharisai5@users.noreply.github.com>
2023-05-11 15:46:59 +0530

    Update .envsetup.sh

--- .envsetup.sh
+++ .envsetup.sh
@@ -20,7 +20,7 @@ source venv/bin/activate
 
 pip3 install -r requirements.txt
 python3 ./demo/manage.py makemigrations
-python3 ./demo/manage.py .migrate
+python3 ./demo/manage.py migrate
 
 sudo ufw allow 8000
 
